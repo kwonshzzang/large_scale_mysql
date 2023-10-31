27609e8462fa1f5d9318e45680acad83be74f975
kwonshzzang <kwonshzzang@gmail.com>
2023-10-31 11:28:25 +0900

    First Commit

---
+++ src/main/java/kr/co/kwonshzzang/largescalemysql/largescalemysql/LargeScaleMysqlApplication.java
@@ -0,0 +1,13 @@
+package kr.co.kwonshzzang.largescalemysql.largescalemysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargeScaleMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LargeScaleMysqlApplication.class, args);
+    }
+
+}
